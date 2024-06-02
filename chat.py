e6e80649cce56e102e93413b826669acd643fcb2
Emmanuel Sibanda <emmanuelsibandaus@gmail.com>
2024-06-02 13:59:01 -0700

    latest version

--- chat.py
+++ chat.py
@@ -6,53 +6,84 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 import dotenv
 import discord
+import models
+import claude
+import anthropic
+import json
+# from discord_slash import SlashCommand
 
 dotenv.load_dotenv()
-bot = discord.Bot()
 test = str(os.getenv("TOKEN"))
 openAI = str(os.getenv("OPENAI"))
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user} is ready and online!")
+conversation = None
+memory = None
+intents = discord.Intents.all() 
+intents.message_content = True
+client = discord.Client(intents = intents)
 
-@bot.event
+guild_ids = [1246523804792914062]
+users = ["Emmanuel"]
+# def build_prompt():
+    
+@client.event
+async def on_ready():
+    print(f"{client.user} is ready and online!")
+
+@client.event
+async def on_guild_channel_create(channel):
+    """This event is triggered when a channel is createed informing all users how the bot works"""
+    welcome_message = """Hey everyone, thanks for inviting me to the group chat!
+    My role here is to help y'all plan an amazing trip together by facilitating the conversation. You can think of think of this like any other group chat, except I'll be here answering research questions and driving the group toward an itinerary that works for everyone.
+    To get us started -- does anyone have thoughts or suggestions for the group?"""
+    await channel.send(welcome_message)
+
+
+@client.event
 async def on_member_join(member):
     await member.send(
         f'Welcome to the server, {member.mention}.'
     )
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Hey!")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    pre_order_convo = []
+    async for message in message.channel.history():
+        if not message.author.global_name: user_name = "bot"
+        else: user_name = message.author.global_name
+        pre_order_convo.append({user_name: message.content})
+    conversation = pre_order_convo[::-1]
+    print(conversation)
+    try:
+        response = 'No calls to the API were made.'
+        users = ["Jay", "Oliver", "Emamanuel"] 
+        research_prompt = models.ConversationState.build_research_prompt(users=users, conversation=conversation)
+        response = claude.prompt(research_prompt)
+        conversation_state = models.ConversationState.model_validate_json(response)
+        if conversation_state.state == 'question':
+            preferences = {} 
+            for user in users:
+                preference_prompt = models.UserPreferences.build_prompt(user=user, conversation=conversation)
+                response = claude.prompt(preference_prompt)
+                preference = models.UserPreferences.model_validate_json(response)
+                preferences[user] = preference
+            research_prompt = models.ResearchPath().build_research_prompt(preferences=preferences)
+            response = claude.prompt(research_prompt)
+            research = models.ResearchPath.model_validate_json(response)
+            response_prompt = research.build_response_prompt(preferences=preferences)
+            response = claude.prompt(response_prompt)
+            print(response)
+            await message.channel.send(response)
+        else:
+            print(conversation_state.state)
+            response_prompt = conversation_state.build_response_prompt(users=users, conversation=conversation)
+            response = claude.prompt(response_prompt)
+            print(response)
+            await message.channel.send(response)
+    except Exception as e:
+        print(response)
+        print(e)
 
-#testing whether bot returns total number of members in server
-@bot.slash_command(name='count', description="Returns the total count of members")
-async def count(ctx: discord.ApplicationContext):
-    member_count = ctx.guild.member_count - 1 
-    await ctx.respond(f'The number of members in this server is {member_count}.')
-
-#needs to be aware of all the people in the group
-#needs to ask each person questions to understand their preferences
-#needs to structure the
-@bot.slash_command(name='start', description="Starts the AI conversation")
-async def preferences(ctx: discord.ApplicationContext):
-    member_count = ctx.guild.member_count - 1
-    template = """
-    You are a travel agent, your task is to understand the preferences of all users in the group
-    history: {history}
-    Human: {input}
-    """
-    prompt_template = PromptTemplate(input_variables=["history", "input"], template=template)
-    context = {
-            "input": f"Ask questions to all {member_count} members in the group.",
-    }
-    llm = ChatOpenAI(model="gpt-3.5-turbo-16k", openai_api_key=openAI)
-    memory = ConversationBufferMemory(memory_key="history")
-    memory_variables = memory.load_memory_variables(inputs=context)
-    context["history"] = memory_variables.get("history", "")
-    conversation = ConversationChain(prompt=prompt_template, llm=llm, memory=memory)
-    response = conversation.predict(input=context["input"], history=context["history"])
-    memory.save_context(inputs={"input": context["input"]}, outputs={"history": response})
-    await ctx.respond(response)
-bot.run(os.getenv('TOKEN'))
+client.run(os.getenv('TOKEN'))
